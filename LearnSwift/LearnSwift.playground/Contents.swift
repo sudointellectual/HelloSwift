1d918a85f25f8adbc7f5496caf00e76f059f0f57
Jessica Rocha Lim <mail@jessicarocha.com>
2015-07-06 23:02:40 -0400

    new exercises

--- LearnSwift/LearnSwift.playground/Contents.swift
+++ LearnSwift/LearnSwift.playground/Contents.swift
@@ -21,5 +21,94 @@ myLabel.textAlignment = .Center
 myLabel.font = UIFont(name: "Georgia", size: 24)
 myLabel
 
+// 7.2
+var myNumber = 10
+var myLetter = "c"
+
+// 7.2.1
+
+println("Int32 Min = \(Int32.min) Int32 Max = \(Int32.max)")
+
+// 7.2.4
+
+var myChar1 = "f"
+var myChar2 = ":"
+var myChar3 = "X"
+var myChar4 = "\u{0058}"
+
+
+// 7.2.5
+
+var userName = "Jess"
+var inboxCount = 9999
+let maxCount = 10000
+
+var message = "\(userName) has \(inboxCount) messages. Message capacity remaining is \(maxCount - inboxCount)"
+println(message)
+
+// 7.2.6
+
+// assigns a new line to variable newline
+var newline = "\n"
+
+// you can escape the backslash character if that's what you actually want
+var backslash = "\\"
+
+// 7.5
+
+// var userCount = 10
+let maxUserCount = 20
+
+// 7.6
+
+var userCount: Int = 10
+var signalStrength = 2.231
+let companyName = "Hello Kitty's Pleasure Palace"
+
+// let bookTitle = "The Extreme Lightnes of Being"
+let bookTitle: String
+
+var crankyOldManBookType = true
+
+if crankyOldManBookType {
+    bookTitle = "The Extreme Lightness of Being"
+} else {
+    bookTitle = "Eat. Pray. Love."
+}
+
+// 7.7
+
+//let myTuple = (10, 432.33, "String theory.")
+// var myString = myTuple.2
+// println(myString)
+// let (myInt, myFloat, myString) = myTuple
+// var (myInt, , myString) = myTuple
+let myTuple = (count: 10, length: 432.433, message: "String Theory")
+println(myTuple.message)
+
+// 7.8
+
+var index: Int?
+if index != nil {
+    // index variable has a value assigned to it
+} else {
+    // index variable has no value assigned to it
+}
+
+index = 3
+
+var treeArray = ["Oak", "Pine", "Yew", "Birch"]
+
+if index != nil {
+    println(treeArray[index!])
+} else {
+    println("index does not contain a value")
+}
+
+
+
+
+
+
 
 
