e83942f9f85cbe3f7d79fa453686f7c1095194d2
Jessica Rocha Lim <mail@jessicarocha.com>
2015-07-06 22:27:56 -0400

    hello swift an intro to playgrounds

---
+++ LearnSwift/LearnSwift.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+println("Welcome to Swift.")
+
+var x = 10
+
+for index in 1...20 {
+    let y = index * x--
+}
+
+
+
+let myLabel = UILabel(frame: CGRectMake(0, 0, 200, 50))
+myLabel.backgroundColor = UIColor.redColor()
+myLabel.text = "Hello Swift."
+myLabel.textAlignment = .Center
+myLabel.font = UIFont(name: "Georgia", size: 24)
+myLabel
+
+
+
