48687676cbae8d007fcf39acb0ddbe7a049f0b36
Jessica Rocha Lim <mail@jessicarocha.com>
2015-07-06 22:16:41 -0400

    my first commit 👾

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Jessica Milanesa Rocha on 7/6/15.
+//  Copyright (c) 2015 Jessica Milanesa Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
