c811bcc593af838bd9a862f1eaff42fc0f7f4353
Jessica Lim <mail@jessicarocha.com>
2015-07-15 15:06:12 -0400

    Chapter 12

--- LearnSwift/LearnSwift-12.playground/Contents.swift
+++ LearnSwift/LearnSwift-12.playground/Contents.swift
@@ -3,3 +3,66 @@
 import UIKit
 
 var str = "Hello, playground"
+
+
+class BankAccount {
+    var accountBalance: Float = 0
+    var accountNumber: Int = 0
+    let fees: Float = 25.00
+    
+    var balanceLessFees: Float {
+        get {
+            return accountBalance - fees
+        }
+        
+        set(newBalance) {
+            accountBalance = newBalance - fees
+        }
+    }
+    
+    init(number: Int, balance: Float) {
+        accountNumber = number
+        accountBalance = balance
+    }
+    
+    deinit {
+        // Perform any necessary cleanup here
+    }
+    
+    func displayBalance () {
+        println("Number \(accountNumber)")
+        println("Current balance is \(accountBalance)")
+    }
+    
+    class func getMaxBalance() -> Float {
+        return 100000.00
+    }
+}
+
+var account1: BankAccount = BankAccount(number:12312312, balance: 400.54)
+var balance1 = account1.balanceLessFees
+
+account1.accountBalance = 6789.98
+account1.displayBalance()
+
+var maxAllowed = BankAccount.getMaxBalance()
+
+class myClass {
+    var myNumber = 1
+    
+    func addTen() {
+        myNumber += 10
+    }
+}
+
+class newClass {
+    var myNumber = 10
+    
+    func addTenAgain(myNumber: Int) {
+        println(myNumber) // Output the function parameter value
+        println(self.myNumber) // Output the class property value
+    }
+}
+
+
+
