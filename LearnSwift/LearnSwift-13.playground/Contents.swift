7575cd68e039865a149aac4b3726944cebbff274
Jessica Lim <mail@jessicarocha.com>
2015-07-22 17:42:46 -0400

    finished chapter 13

--- LearnSwift/LearnSwift-13.playground/Contents.swift
+++ LearnSwift/LearnSwift-13.playground/Contents.swift
@@ -3,18 +3,30 @@
 import UIKit
 
 class BankAccount {    var accountBalance: Float    var accountNumber: Int
-        init(number: Int, balance: Float) {
-    accountNumber = number    accountBalance = balance    }
+        init(number: Int, balance: Float) {        accountNumber = number        accountBalance = balance    }
         func displayBalance() {        println("Number \(accountNumber)")        println("Current balance is \(accountBalance)")
     }}
 
 class SavingsAccount: BankAccount {
     var interestRate: Float
         
+        init(number: Int, balance: Float, rate: Float) {
+            interestRate = rate
+            super.init(number: number, balance: balance)
+        }
+        
     func calculateInterest() -> Float {
         return interestRate * accountBalance
     }
-        
+            
+    override func displayBalance() {
+    super.displayBalance()        println("Prevailing interest rate is \(interestRate)")
+    }
     
 }
+
+var savings1 = SavingsAccount(number: 12311, balance: 600.00, rate: 0.07)
+
+println(savings1.calculateInterest())
+savings1.displayBalance()
 
