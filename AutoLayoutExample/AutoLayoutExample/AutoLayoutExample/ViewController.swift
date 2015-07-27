0526f231efcc3ed5cfe17f73217e9d5047964015
Jessica Lim <mail@jessicarocha.com>
2015-07-27 15:10:09 -0400

    chapters 19 and 20

---
+++ AutoLayoutExample/AutoLayoutExample/AutoLayoutExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutExample
+//
+//  Created by Jessica Lim on 7/27/15.
+//  Copyright (c) 2015 Jessica Lim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
