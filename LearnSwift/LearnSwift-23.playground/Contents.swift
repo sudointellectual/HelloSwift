01fd08187c62be715bb6fc00b7bac69a5ae29451
Jessica Lim <mail@jessicarocha.com>
2015-08-03 17:12:07 -0400

    that time i really screwed up chapter 24

---
+++ LearnSwift/LearnSwift-23.playground/Contents.swift
@@ -0,0 +1,32 @@
+
+
+import UIKit
+
+var str = "Hello, playground"
+
+let superview = self.view
+
+let myLabel = UILabel()
+myLabel.setTranslatesAutoresizingMaskIntoConstraints(false)
+myLabel.text = "My Label"
+
+let myButton = UIButton()
+myButton.backgroundColor = UIColor.redColor()
+myButton.setTitle("My Button", forState: UIControlState.Normal)
+myButton.setTranslatesAutoresizingMaskIntoConstraints(false)
+
+superview.addSubview(myLabel)
+superview.addSubview(myButton)
+
+let viewsDictionary = ["myLabel": myLabel, "myButton": myButton]
+
+superview.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat( "V:|- [myButton]-|",    options: NSLayoutFormatOptions.AlignAllBaseline,
+    metrics: nil,    views: viewsDictionary))
+
+superview.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("|-[myButton]-[myLabel(==myButton)]-|",
+    options: NSLayoutFormatOptions.AlignAllBaseline,
+    metrics: nil,
+    views: viewsDictionary))
+
+
+
